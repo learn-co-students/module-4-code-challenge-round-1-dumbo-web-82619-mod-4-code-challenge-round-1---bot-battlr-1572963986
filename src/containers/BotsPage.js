ac135f9c20ac367094369de3422e477746de731b
github username <github email address>
2019-11-05 10:06:45 -0500

    Final commit

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,14 +1,30 @@
 import React from "react";
+import YourBotArmy from './YourBotArmy'
+import BotCollection from './BotCollection'
+
+const API =  'https://bot-battler-api.herokuapp.com/api/v1/bots'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: []
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy/>
+        <BotCollection/>
       </div>
     );
+  }
+
+  componentDidMount(){
+    fetch(API)
+      .then(resp => resp.json())
+      .then(botsResp =>{
+        this.setState({bots: botsResp}})
   }
 
 }
