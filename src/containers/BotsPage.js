5fbeeeb0779e6cfd7320cb6cf2cf0608f181b564
NMontesino <namontesino@gmail.com>
2019-11-05 11:09:10 -0500

    Probably didn't pass but did better than expected

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,62 @@
 import React from "react";
+import BotCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = 
+  {
+    bots: [],
+    armyBots: [],
+    uniqueArmyBots: []
+  }
+
+  componentDidMount()
+  {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then((bots) => 
+    {
+
+      this.setState(
+      {
+        bots: bots
+      })
+
+    })
+
+    const uniqueBots = []
+    this.state.armyBots.map((bot) => 
+    {
+      
+      if(uniqueBots.indexOf(bot) === -1)
+      {
+        uniqueBots.push(bot)
+      }
+
+    })
+    this.setState(
+    {
+      uniqueArmyBots: uniqueBots
+    })
+  }
+
+  addToArmy = (bot) => 
+  {
+    this.setState(
+    {
+      armyBots: [...this.state.armyBots, bot]
+    })
+  }
+
+
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy armyBots={ this.state.uniqueArmyBots } />
+        <BotCollection bots={ this.state.bots } enlist={ this.addToArmy } />
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -5,12 +5,17 @@ class YourBotArmy extends React.Component {
   //your bot army code here...
 
   render(){
+    const botCards = this.props.armyBots.map((bot) => 
+		{
+			return <BotCard bot={ bot } />
+		})
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
             {/*...and here...*/}
             Your Bot Army
+            { botCards }
           </div>
         </div>
       </div>
